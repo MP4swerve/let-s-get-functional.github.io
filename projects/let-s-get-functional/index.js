97afcd0d04bba5a60628ce0a27bd98cc80216d91
MP4swerve <key.investnola@gmail.com>
2024-02-27 18:28:12 -0600

    uio

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -59,7 +59,7 @@ var youngestCustomer = function(array){
 
 var averageBalance = function(array){
     var sum = _.reduce(array, function(acc, current){
-        return acc + current
+        return acc.balance + current.balance
     })
     var avg = sum / array.length;
     return avg;
